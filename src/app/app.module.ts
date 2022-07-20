3df45198ba008ccd18807f3b54b835fbcbab9114
Alessandro <alessandrocosta103@gmail.com>
2022-07-20 19:14:59 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { AppComponent } from './app.component';
+import { ListApiComponent } from './list-api/list-api.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListApiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
